9f72ff81ba1e2f0db14c469531904ec8cd8cb745
asmitamthorat <asmita26296@gmail.com>
2020-08-28 12:22:15 +0530

    solving using switch case statment

---
+++ empWageCalculation_DAY21.java
@@ -0,0 +1,29 @@
+package javaPractice;
+
+public class empWageCalculation_DAY21 {
+
+	public static final int isFullTime=2;
+	public static final int isPartTime=1;
+	public static final int empRatePerHour=20;
+	public static void main (String[] args) {
+		//Variables
+		int empHours=0;
+		int empWage=0;
+		//Computation
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+			case isFullTime:
+				empHours=8;
+				break;
+			case isPartTime:
+				empHours=4;
+				break;
+			default:
+				empHours=0;
+		}
+				
+		empWage=empHours*empRatePerHour;
+		System.out.println("Emp Wage: " +empWage);		//printing the employee wage
+	}
+
+}
