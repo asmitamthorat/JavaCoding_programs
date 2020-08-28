db1477c2e2d403c42bef2af2defd593e9cf6cf5e
asmitamthorat <asmita26296@gmail.com>
2020-08-28 15:00:28 +0530

    solving using case statment

---
+++ empWageCalculation_DAY22.java
@@ -0,0 +1,39 @@
+package javaPractice;
+
+/************************************************************************************************
+ * purpose: To perform Employee wage computation
+ *
+ * 
+ * @author asmita thorat
+ *
+ *
+ *since 25/08/2020
+ ***********************************************************************************************/
+
+public class empWageCalculation_DAY22 {
+
+	public static final int isFullTime=2;
+	public static final int isPartTime=1;
+	public static final int empRatePerHour=20;
+	public static void main (String[] args) {
+		//Variables
+		int empHours=0;
+		int empWage=0;
+		//Computation
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+			case isFullTime:
+				empHours=8;
+				break;
+			case isPartTime:
+				empHours=4;
+				break;
+			default:
+				empHours=0;
+		}
+				
+		empWage=empHours*empRatePerHour;
+		System.out.println("Emp Wage: " +empWage);		//printing the employee wage
+
+	}
+}
