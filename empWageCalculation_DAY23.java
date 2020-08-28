6adcfe2865c3536afb24c6107e16dde766c79292
asmitamthorat <asmita26296@gmail.com>
2020-08-28 18:04:45 +0530

    checking employee is present or not

--- empWageCalculation_DAY23.java
+++ empWageCalculation_DAY23.java
@@ -2,9 +2,15 @@ package javaPractice;
 
 public class empWageCalculation_DAY23 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main (String[] args) {
+		int CheckPresence = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == CheckPresence) {						//checking whether employee is present or absent
+			System.out.println("employee is present");
+		}
+		else {
+			System.out.println("employee is absent");
+		}
 	}
 
 }
