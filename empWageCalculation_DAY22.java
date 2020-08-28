48ca8cf95999c91844d53100bc3e9f59a2015136
asmitamthorat <asmita26296@gmail.com>
2020-08-28 18:11:56 +0530

    employee wages for multiple companies

--- empWageCalculation_DAY22.java
+++ empWageCalculation_DAY22.java
@@ -1,7 +1,7 @@
 package javaPractice;
 
 /************************************************************************************************
- * purpose: To perform Employee wage computation
+ * purpose: To perform Employee wage calculation
  *
  * 
  * @author asmita thorat
@@ -12,24 +12,46 @@ package javaPractice;
 
 public class empWageCalculation_DAY22 {
 
-			public static void main (String[] args) {
-
-			int isFullTime=2;
-			int isPartTime=1;
-			int emp_rate_per_hour=20;
-			int empHours=0;
-			int empWage=0;
-			double empCheck = Math.floor(Math.random()*10)%3;		////computation of random number to check shift
-			if (empCheck == isFullTime) {
-				empHours=8;
-			}
-			else if  (empCheck==isPartTime){
-				empHours=4;
-			}
-			else {
-				empHours=0;
-			}
-			empWage=empHours*emp_rate_per_hour;
-			System.out.println("Emp Wage: " +empWage);				//printing the employee wage
+	public static final int isPartTime=1;
+	public static final int isFullTime=2;
+	
+	public static int computeEmpWage(String company, int empRatePerHour,int numOf_workingDays, int maxHrs_inMonth) {
+		//Variables
+		int empHours=0;
+		int empWage=0;
+	
+		int totalWorkingDays=0;
+		int totalEmpHour=0;
+		//Computation
+		while (totalEmpHour<=maxHrs_inMonth &&  totalWorkingDays<numOf_workingDays) {
+			totalWorkingDays=totalWorkingDays+1;
+			int empCheck = (int) Math.floor(Math.random()*10)%3;
+			switch (empCheck) {							// checking whether employee is full time ,part time or absent
+				case isFullTime:
+					empHours=8;
+					break;
+				case isPartTime:
+					empHours=4;
+					break;
+				default:
+					empHours=0;
 		}
+			totalEmpHour=totalEmpHour+empHours;
+			System.out.println("Days#:"+totalWorkingDays + "Emp Hr: " +empHours);
+			
+			
+				
+			
+	}			
+		int totalEmpWage=totalEmpHour*empRatePerHour;
+		System.out.println("Total employee wage: " +company+" is : "+totalEmpWage);	//printing the employee wage
+		return totalEmpWage;
+		
+	}
+	
+	public static void main(String[] args) {
+		computeEmpWage("Dmart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
+	}
+
 }
