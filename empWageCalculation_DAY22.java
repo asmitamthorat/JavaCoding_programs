45fa6e2b80a87feba1a01fc34ca7e451c69b627d
asmitamthorat <asmita26296@gmail.com>
2020-08-28 14:51:40 +0530

    adding part time employee and wages

---
+++ empWageCalculation_DAY22.java
@@ -0,0 +1,35 @@
+package javaPractice;
+
+/************************************************************************************************
+ * purpose: To perform Employee wage computation
+ *
+ * 
+ * @author asmita thorat
+ *
+ *
+ *since 25/08/2020
+ ***********************************************************************************************/
+
+public class empWageCalculation_DAY22 {
+
+	public static void main (String[] args) {
+
+		int isFullTime=2;
+		int isPartTime=1;
+		int emp_rate_per_hour=20;
+		int empHours=0;
+		int empWage=0;
+		double empCheck = Math.floor(Math.random()*10)%3;		////computation of random number to check shift
+		if (empCheck == isFullTime) {
+			empHours=8;
+		}
+		else if  (empCheck==isPartTime){
+			empHours=4;
+		}
+		else {
+			empHours=0;
+		}
+		empWage=empHours*emp_rate_per_hour;
+		System.out.println("Emp Wage: " +empWage);				//printing the employee wage
+	}
+}
