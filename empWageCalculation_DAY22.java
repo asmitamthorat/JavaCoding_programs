600f9799e8e4c40f8eeb5b260f88a04fcbf91693
asmitamthorat <asmita26296@gmail.com>
2020-08-28 14:54:30 +0530

    adding part time employee and wages

--- empWageCalculation_DAY22.java
+++ empWageCalculation_DAY22.java
@@ -12,20 +12,24 @@ package javaPractice;
 
 public class empWageCalculation_DAY22 {
 
-	public static void main (String[] args) {
+			public static void main (String[] args) {
 
-		int isFullTime=1;
-		int emp_rate_per_hour=20;
-		int empHours=0;
-		int empWage=0;
-		double empCheck = Math.floor(Math.random()*10)%2;   //computation to generate random for employee shift
-		if (empCheck == isFullTime) {
-			empHours=8;
+			int isFullTime=2;
+			int isPartTime=1;
+			int emp_rate_per_hour=20;
+			int empHours=0;
+			int empWage=0;
+			double empCheck = Math.floor(Math.random()*10)%3;		////computation of random number to check shift
+			if (empCheck == isFullTime) {
+				empHours=8;
+			}
+			else if  (empCheck==isPartTime){
+				empHours=4;
+			}
+			else {
+				empHours=0;
+			}
+			empWage=empHours*emp_rate_per_hour;
+			System.out.println("Emp Wage: " +empWage);				//printing the employee wage
 		}
-		else {
-			empHours=0;
-		}
-		empWage=empHours*emp_rate_per_hour;
-		System.out.println("Emp Wage: " +empWage);			//printing the employee wage
-	}
 }
