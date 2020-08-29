b308c950f99982d0c1231800daa19343add40797
asmitamthorat <asmita26296@gmail.com>
2020-08-29 11:04:10 +0530

    computing wages of multiple companies using interface approach

--- empWageCalculation_DAY23.java
+++ empWageCalculation_DAY23.java
@@ -10,33 +10,61 @@ package javaPractice;
  *since 25/08/2020
  ***********************************************************************************************/
 
-public class empWageCalculation_DAY23 {
+class CompanyEmpWage {
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
+	public int totalEmpWage;
 
-	public static final int isPartTime=1;
-	public static final int isFullTime=2;
-	
-	private final String company;
-	private final int empRatePerHour;
-	private final int numOfWorkingDays;
-	private final int maxHoursPerMonth;
-	private int totalEmpWage;
-	
-	public empWageCalculation_DAY23(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth) {
-		//Variables
-		
-		this.company = company;
+	public CompanyEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company=company;
 		this.empRatePerHour=empRatePerHour;
-		this.numOfWorkingDays=numOfWorkingDays;
+		this.numOfWorkingDays= numOfWorkingDays;
 		this.maxHoursPerMonth=maxHoursPerMonth;
 	}
 	
-	public void computeEmpWage() {
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage=totalEmpWage;
+	}
 	
+	public String toString() {
+		return "Total Emp Wage for Company:" +company+ " is :"+totalEmpWage;
+	}
+}	
+
+public class empWageCalculation_DAY23{
+	public static final int isPartTime=1;
+	public static final int isFullTime=2;
+	
+	private int numOfCompany=0;
+	private CompanyEmpWage[] companyEmpWageArray;
+	
+	public empWageCalculation_DAY23() {
+		companyEmpWageArray=new CompanyEmpWage[5];
+	}
+	
+	private void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		
+		companyEmpWageArray[numOfCompany]=new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+		numOfCompany++;
+		
+	}
+	
+	private void computeEmpWage() {
+		for (int i=0; i<numOfCompany; i++) {
+			
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
+	
+	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 		int empHrs=0;
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
 		//Computation
-		while (totalEmpHrs<=maxHoursPerMonth && totalWorkingDays<numOfWorkingDays) {
+		while (totalEmpHrs<=companyEmpWage.maxHoursPerMonth && totalWorkingDays<companyEmpWage.numOfWorkingDays) {
 			totalWorkingDays=totalWorkingDays+1;
 			int empCheck = (int) Math.floor(Math.random()*10)%3;
 			switch (empCheck) {							// checking whether employee is full time ,part time or absent
@@ -51,23 +79,14 @@ public class empWageCalculation_DAY23 {
 			}
 			totalEmpHrs=totalEmpHrs+empHrs;
 			System.out.println("Days :"+totalWorkingDays + "Emp Hr: " +empHrs);
-					
-		}	
-		totalEmpWage=totalEmpHrs * empRatePerHour;
-		
-	}	
-		public String toString() {
-			return "total Emp Wage for Company:" +company+ " is :"+totalEmpWage;
+		}
+		return totalEmpHrs*companyEmpWage.empRatePerHour;
 	}
 	
 	public static void main(String[] args) {
-		empWageCalculation_DAY23 bazar=new empWageCalculation_DAY23("bazar",20,2,10);  //creating object
-		empWageCalculation_DAY23 mall=new empWageCalculation_DAY23("mall",20,2,10);  //creating a object reliance
-		bazar.computeEmpWage();
-		System.out.println(bazar);
-		mall.computeEmpWage();
-		System.out.println(mall);
-		
+		empWageCalculation_DAY23 obj = new empWageCalculation_DAY23();
+		obj.addCompanyEmpWage("Dmart", 20, 2, 10);
+		obj.addCompanyEmpWage("Reliance", 10, 4, 10);
+		obj.computeEmpWage();
 	}
-
 }
